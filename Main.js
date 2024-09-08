354248e7aee6041ff1a67ee65358adc17d7f23b5
Alveena-Ali <163353918+Alveena-Ali@users.noreply.github.com>
2024-09-08 19:45:22 +0500

    Que 26: Alien Colors #2

---
+++ Main.js
@@ -0,0 +1,25 @@
+"use strict";
+// Que 26: Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+// • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+Object.defineProperty(exports, "__esModule", { value: true });
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+// • Write one version of this program that runs the if block and another that runs the else block.
+// ==========================================================================================
+// Choose a color for an alien as you did in Exercise 25
+let alienColors = "green";
+// write an if-else chain.
+// • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+if (alienColors === "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+alienColors = "yellow";
+if (alienColors === "green") {
+    console.log("The player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
